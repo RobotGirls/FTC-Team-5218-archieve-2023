4b32bfca2e0a7baaa88d2a5c3c4b477578d57c76
test <test@wpp-macbook-pro-20180108-4.local>
2021-01-05 19:16:29 -0800

    Created test program for controlling double launch mechanism.

---
+++ 2021/test/LaunchMechTest.java
@@ -0,0 +1,149 @@
+/*
+ * Copyright (c) September 2017 FTC Teams 25/5218
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without modification,
+ *  are permitted (subject to the limitations in the disclaimer below) provided that
+ *  the following conditions are met:
+ *
+ *  Redistributions of source code must retain the above copyright notice, this list
+ *  of conditions and the following disclaimer.
+ *
+ *  Redistributions in binary form must reproduce the above copyright notice, this
+ *  list of conditions and the following disclaimer in the documentation and/or
+ *  other materials provided with the distribution.
+ *
+ *  Neither the name of FTC Teams 25/5218 nor the names of their contributors may be used to
+ *  endorse or promote products derived from this software without specific prior
+ *  written permission.
+ *
+ *  NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ *  LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  AS IS AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESSFOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ *  DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ *  SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ *  TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ *  THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package test;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import team25core.DeadmanMotorTask;
+import team25core.GamepadTask;
+import team25core.OneWheelDirectDrivetrain;
+import team25core.Robot;
+import team25core.RobotEvent;
+import team25core.RunToEncoderValueTask;
+import team25core.SingleShotTimerTask;
+import team25core.StandardFourMotorRobot;
+import team25core.TankMechanumControlSchemeReverse;
+import team25core.TeleopDriveTask;
+
+@TeleOp(name = "LaunchMechTest")
+//@Disabled
+public class LaunchMechTest extends Robot {
+
+
+    private Telemetry.Item linearPos;
+    private Telemetry.Item launchMechTelem;
+
+    //private TeleopDriveTask drivetask;
+    private DcMotor launchMechRight;
+    private DcMotor launchMechLeft;
+    //private FourWheelDirectDrivetrain drivetrain;
+    //private MechanumGearedDrivetrain drivetrain;
+
+    private static final int TICKS_PER_INCH = 79;
+
+    @Override
+    public void handleEvent(RobotEvent e) {
+    }
+
+    @Override
+    public void init() {
+
+        super.init();
+        launchMechTelem = telemetry.addData("launch Mech Power", 0);
+        //mapping the wheels
+//        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+//        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+//        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+//        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        //mapping the launch mech and intake mech
+        launchMechRight = hardwareMap.get(DcMotor.class, "launchMechRight");
+        launchMechLeft = hardwareMap.get(DcMotor.class, "launchMechLeft");
+
+        //**CONTINUE FROM HERE**
+
+        // using encoders to record ticks
+//        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+//        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launchMechRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        launchMechLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+       /* launch = new OneWheelDirectDrivetrain(launchMech);
+        launch.resetEncoders();
+        launch.encodersOn();
+
+        intake = new OneWheelDirectDrivetrain(intakeMech);
+        intake.resetEncoders();
+        intake.encodersOn();
+
+        */
+
+//        TankMechanumControlSchemeReverse scheme = new TankMechanumControlSchemeReverse(gamepad1);
+
+        //code for forward mechanum drivetrain:
+        //drivetrain = new MechanumGearedDrivetrain(360, frontRight, rearRight, frontLeft, rearLeft);
+    }
+
+    @Override
+    public void start() {
+
+//        TankMechanumControlSchemeReverse scheme = new TankMechanumControlSchemeReverse(gamepad1);
+//
+//        drivetask = new TeleopDriveTask(this, scheme, frontLeft, frontRight, backLeft, backRight);
+//
+//        this.addTask(drivetask);
+
+        //gamepad 1
+        this.addTask(new GamepadTask(this, GamepadTask.GamepadNumber.GAMEPAD_1) {
+            //@Override
+            public void handleEvent(RobotEvent e) {
+                GamepadEvent gamepadEvent = (GamepadEvent) e;
+
+                switch (gamepadEvent.kind) {
+                    case BUTTON_X_DOWN:
+                        // enable the launch mech
+                        launchMechRight.setPower(1.0);
+                        launchMechLeft.setPower(-1.0);
+                        launchMechTelem.setValue(1.0);
+                        break;
+                    case BUTTON_X_UP:
+                        // stop the launch mech
+                        launchMechRight.setPower(0);
+                        launchMechLeft.setPower(0);
+                        break;
+                }
+            }
+        });
+    }
+}
+
