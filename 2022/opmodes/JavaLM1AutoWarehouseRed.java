63d23de55933a7b5df6d5feea47da5f3eeaa7e9e
shreyas61 <shreyasriram@rocknrollrobots25.com>
2021-11-06 15:31:39 -0700

    Created an autonomous that strafes into warehouse, need to test before LM1

---
+++ 2022/opmodes/JavaLM1AutoWarehouseRed.java
@@ -0,0 +1,161 @@
+/*
+Copyright (c) September 2017 FTC Teams 25/5218
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of FTC Teams 25/5218 nor the names of their contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+package opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.RobotLog;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+import team25core.DeadReckonPath;
+import team25core.DeadReckonTask;
+import team25core.FourWheelDirectDrivetrain;
+import team25core.OneWheelDirectDrivetrain;
+import team25core.Robot;
+import team25core.RobotEvent;
+import team25core.SingleShotTimerTask;
+
+
+@Autonomous(name = "JavaLM1AutoWarehouseRed")
+//@Disabled
+public class JavaLM1AutoWarehouseRed extends Robot {
+
+    private final static int CAROUSEL_TIMER = 40;
+    //private Telemetry.Item loggingTlm;
+    private Telemetry.Item objectSeenTlm;
+
+    private DcMotor frontLeft;
+    private DcMotor frontRight;
+    private DcMotor backLeft;
+    private DcMotor backRight;
+
+    private DcMotor carouselMech;
+    private OneWheelDirectDrivetrain singleMotorDrivetrain;
+    private DcMotor liftMotor;
+    private DcMotor intakeMotor;
+
+    //private MechanumGearedDrivetrain drivetrain;
+    private FourWheelDirectDrivetrain drivetrain;
+
+    private Telemetry.Item timerTlm;
+    private Telemetry.Item handleEventTlm;
+
+    SingleShotTimerTask carouselTimerTask;
+    DeadReckonPath wareHousePath;
+
+    /*
+     * The default event handler for the robot.
+     */
+    @Override
+    public void handleEvent(RobotEvent e)
+    {
+        /*
+         * Every time we complete a segment drop a note in the robot log.
+         */
+        if (e instanceof DeadReckonTask.DeadReckonEvent) {
+            RobotLog.i("Completed path segment %d", ((DeadReckonTask.DeadReckonEvent)e).segment_num);
+        }
+    }
+
+
+    public void initPaths()
+    {
+        wareHousePath = new DeadReckonPath();
+
+        wareHousePath.stop();
+
+        wareHousePath.addSegment(DeadReckonPath.SegmentType.SIDEWAYS, 20, 0.5);
+
+    }
+
+    public void goToWareHouse()
+    {
+        //loggingTlm.setValue("goToCarousel");
+        /*
+         * Alternatively, this could be an anonymous class declaration that implements
+         * handleEvent() for task specific event handlers.
+         */
+
+        this.addTask(new DeadReckonTask(this, wareHousePath, drivetrain){
+            @Override
+            public void handleEvent (RobotEvent e){
+                DeadReckonEvent path = (DeadReckonEvent) e;
+                if (path.kind == EventKind.PATH_DONE)
+                {
+                    RobotLog.i("strafed into WareHouse");
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void init()
+    {
+        //loggingTlm.addData("method", "unknown");
+
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        carouselMech = hardwareMap.get(DcMotor.class, "carouselMech");
+        liftMotor = hardwareMap.get(DcMotor.class,"liftMotor");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakeMotor");
+
+        carouselMech.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        drivetrain = new FourWheelDirectDrivetrain(frontRight, backRight, frontLeft, backLeft);
+        drivetrain.resetEncoders();
+        drivetrain.encodersOn();
+
+        singleMotorDrivetrain = new OneWheelDirectDrivetrain(carouselMech);
+        singleMotorDrivetrain.resetEncoders();
+        singleMotorDrivetrain.encodersOn();
+
+        initPaths();
+    }
+
+    @Override
+    public void start()
+    {
+        goToWareHouse();
+    }
+}
